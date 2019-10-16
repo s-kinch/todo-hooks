213f0b6c1e201cd8ed5f5ebed0375ae9f6b6b326
Sinclair Kinch <sinclair.kinch@us.sogeti.com>
2019-10-16 19:59:18 -0400

    complete todos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 import React, {useState} from 'react';
 import './App.css';
 
+const Todo = ({ todo, completeTodo }) => {
+  return (
+    <div 
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={completeTodo}>Complete</button>
+      </div>
+    </div>
+  )
+}
+
+const TodoForm = ({ addTodo, completeTodo }) => {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!value) return
+    addTodo(value)
+    setValue("")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text"
+        className="input"
+        value={value} 
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  )
+}
+
 function App() {
   const [todos, setTodos] = useState([
-    { text: "Pack for Buffalo" },
-    { text: "Learn hooks" },
-    { text: "Scare away this gnat" }
+    { text: "Pack for Buffalo", isCompleted: false },
+    { text: "Learn hooks", isCompleted: false },
+    { text: "Scare away this gnat", isCompleted: false }
   ]);
 
-  // we'll render our todos here ...
-  return <div></div>
+  const addTodo = (text) => setTodos([...todos, { text, isCompleted: false }])
+  const completeTodo = (index) => setTodos([...todos.slice(0, index), {...todos[index], isCompleted: true}, ...todos.slice(index + 1)])
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={() => completeTodo(index)}
+          />
+        ))}
+        <TodoForm {...{addTodo}} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
